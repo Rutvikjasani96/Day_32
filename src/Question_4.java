e7099250d11b7286a0775ca90f15c73d0592886e
Rutvik_Jasani <Rutvikjasani96@users.noreply.github.com>
2024-03-02 15:07:32 +0530

    Day 32 Completed.

--- src/Question_4.java
+++ src/Question_4.java
@@ -1,5 +1,3 @@
-import java.lang.reflect.Array;
-
 public class Question_4 {
 //    given n array elements, choose 2 indices i,j such that i!=j && {arr[i] & arr[j]} is max. (Bitwise &)
     public static void main(String[] args) {
